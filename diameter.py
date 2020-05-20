c07399d0ca420f1396fdb01c93abbfe614161386
Grant Skaggs <grant.skaggs@outlook.com>
2020-05-20 14:24:16 -0500

    added pixel width config

--- diameter.py
+++ diameter.py
@@ -1,13 +1,13 @@
 from PIL import Image
 
 command_line = input().split()
-command_line_names = ["Photo Name", "Epsilon"]
+command_line_names = ["Photo Name", "Pixel Width (mm)", "Epsilon"]
 for arg in zip(command_line_names, command_line):
     print(arg)
 
 filename = command_line[0]
-epsilon = float(command_line[1])
-
+pxl_width = float(command_line[1])
+epsilon = float(command_line[2])
 
 
 
