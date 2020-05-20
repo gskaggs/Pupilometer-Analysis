cebf90df609836699526ba810ac7b7e9a2e57a89
Grant Skaggs <grant.skaggs@outlook.com>
2020-05-20 14:20:32 -0500

    updated command line args presentation

--- diameter.py
+++ diameter.py
@@ -1,7 +1,9 @@
 from PIL import Image
 
 command_line = input().split()
-print("Config:", command_line)
+command_line_names = ["Photo Name", "Epsilon"]
+for arg in zip(command_line_names, command_line):
+    print(arg)
 
 filename = command_line[0]
 epsilon = float(command_line[1])
